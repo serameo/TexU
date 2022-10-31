fc7af453bd1baa9fd1f5eaface8b876f3fa36f3a
Seree Rakwong <meo.rakwong@hotmail.com>
2022-10-31 14:10:03 +0700

    Add files via upload

--- include/texust.h
+++ include/texust.h
@@ -9,223 +9,214 @@
 #ifndef _TEXUSTRUCT_H_
 #define _TEXUSTRUCT_H_
 
-
 #include "texumacros.h"
 #include "texutypes.h"
 #include "texuconst.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-struct texu_env;
-typedef struct texu_env texu_env;
+    struct texu_env;
+    typedef struct texu_env texu_env;
 
-struct texu_wnd;
-typedef struct texu_wnd texu_wnd;
+    struct texu_wnd;
+    typedef struct texu_wnd texu_wnd;
 
+    struct texu_wnd_attrs
+    {
+        texu_i32 y;
+        texu_i32 x;
+        texu_i32 height;
+        texu_i32 width;
+        texu_ui32 style;
+        texu_ui32 exstyle;
+        texu_bool enable;
+        texu_bool visible;
+        const texu_char *text;
+        texu_i32 normalcolor;
+        texu_i32 disabledcolor;
+        texu_i32 focuscolor;
+        texu_ui32 id;
+        const texu_char *clsname;
+        void *userdata;
+    };
+    typedef struct texu_wnd_attrs texu_wnd_attrs;
 
-struct texu_wnd_attrs
-{
-  texu_i32         y;
-  texu_i32         x;
-  texu_i32         height;
-  texu_i32         width;
-  texu_ui32        style;
-  texu_ui32        exstyle;
-  texu_bool        enable;
-  texu_bool        visible;
-  const texu_char*       text;
-  texu_i32         normalcolor;
-  texu_i32         disabledcolor;
-  texu_i32         focuscolor;
-  texu_ui32        id;
-  const texu_char*       clsname;
-  void*            userdata;
-};
-typedef struct texu_wnd_attrs texu_wnd_attrs;
+    struct texu_rect
+    {
+        texu_i32 y;
+        texu_i32 x;
+        texu_i32 lines;
+        texu_i32 cols;
+    };
+    typedef struct texu_rect texu_rect;
 
-struct texu_rect
-{
-  texu_i32        y;
-  texu_i32        x;
-  texu_i32        lines;
-  texu_i32        cols;
-};
-typedef struct texu_rect texu_rect;
+    struct texu_wnd_notify
+    {
+        texu_wnd *wnd;
+        texu_ui32 id;
+        texu_ui32 code;
+    };
+    typedef struct texu_wnd_notify texu_wnd_notify;
 
-struct texu_wnd_notify
-{
-  texu_wnd*        wnd;
-  texu_ui32        id;
-  texu_ui32        code;
-};
-typedef struct texu_wnd_notify texu_wnd_notify;
+    struct texu_wnd_keycmd
+    {
+        texu_i32 key;
+        texu_i32 alt;
+        texu_ui32 cmd;
+    };
+    typedef struct texu_wnd_keycmd texu_wnd_keycmd;
 
-struct texu_wnd_keycmd
-{
-  texu_i32        key;
-  texu_i32        alt;
-  texu_ui32       cmd;
-};
-typedef struct texu_wnd_keycmd texu_wnd_keycmd;
+    struct texu_lbwnd_notify
+    {
+        texu_wnd_notify hdr;
+        texu_i32 index;
+    };
+    typedef struct texu_lbwnd_notify texu_lbwnd_notify;
 
-struct texu_lbwnd_notify
-{
-  texu_wnd_notify  hdr;
-  texu_i32         index;
-};
-typedef struct texu_lbwnd_notify texu_lbwnd_notify;
+    struct texu_cbwnd_notify
+    {
+        texu_wnd_notify hdr;
+        texu_i32 index;
+    };
+    typedef struct texu_cbwnd_notify texu_cbwnd_notify;
 
-struct texu_cbwnd_notify
-{
-  texu_wnd_notify  hdr;
-  texu_i32         index;
-};
-typedef struct texu_cbwnd_notify texu_cbwnd_notify;
+    struct texu_wnd_subitem
+    {
+        texu_i32 idx; /* row index, zero based    */
+        texu_i32 col; /* column index, zero based */
+        texu_char *text;
+        texu_i32 normcolor; /* text attributes          */
+        texu_i32 discolor;  /* text attributes          */
+        texu_i32 selcolor;  /* text attributes          */
+        void *data;         /* user data                */
+    };
+    typedef struct texu_wnd_subitem texu_wnd_subitem;
 
-struct texu_wnd_subitem
-{
-  texu_i32        idx;      /* row index, zero based    */
-  texu_i32        col;      /* column index, zero based */
-  texu_char*      text;
-  texu_i32        normcolor;    /* text attributes          */
-  texu_i32        discolor;    /* text attributes          */
-  texu_i32        selcolor;    /* text attributes          */
-  void*           data;     /* user data                */
-};
-typedef struct texu_wnd_subitem texu_wnd_subitem;
+    struct texu_header_notify
+    {
+        texu_wnd_notify hdr; /* notification header */
+        texu_i32 row;
+        texu_i32 col;
+    };
+    typedef struct texu_header_notify texu_header_notify;
 
-struct texu_header_notify
-{
-  texu_wnd_notify  hdr;         /* notification header */
-  texu_i32         row;
-  texu_i32         col;
-};
-typedef struct texu_header_notify texu_header_notify;
+    struct texu_wnd_header
+    {
+        texu_char *caption;
+        texu_i32 cols;
+        texu_i32 align;      /* column alignment         */
+        texu_i32 normcolor;  /* text attributes          */
+        texu_i32 discolor;   /* text attributes          */
+        texu_i32 selcolor;   /* text attributes          */
+        texu_ui32 editstyle; /* edit style, see TES_XXX  */
+        texu_i32 decwidth;   /* TES_DECIMAL or TES_AUTODECIMALCOMMA, default 6 */
+    };
+    typedef struct texu_wnd_header texu_wnd_header;
 
+    /* edit */
+    struct texu_editminmax
+    {
+        texu_i32 min;
+        texu_i32 max;
+    };
+    typedef struct texu_editminmax texu_editminmax;
 
-struct texu_wnd_header
-{
-  texu_char*        caption;
-  texu_i32          cols;
-  texu_i32          align;      /* column alignment         */
-  texu_i32          normcolor;    /* text attributes          */
-  texu_i32          discolor;    /* text attributes          */
-  texu_i32          selcolor;    /* text attributes          */
-  texu_ui32         editstyle;  /* edit style, see TES_XXX  */
-  texu_i32          decwidth;   /* TES_DECIMAL or TES_AUTODECIMALCOMMA, default 6 */
-};
-typedef struct texu_wnd_header texu_wnd_header;
+    /* tree */
 
-/* edit */
-struct texu_editminmax
-{
-  texu_i32         min; 
-  texu_i32         max; 
-};
-typedef struct texu_editminmax texu_editminmax;
+    struct texu_treewnd_item
+    {
+        texu_char itemtext[TEXU_MAX_WNDTEXT + 1];
+        texu_i64 lparam;
+        texu_bool expanded;
+        texu_bool selected;
+        texu_i32 children;
+        texu_bool populated;
+        texu_i32 normcolor;
+        texu_i32 discolor;
+        texu_i32 selcolor;
+    };
+    typedef struct texu_treewnd_item texu_treewnd_item;
 
-/* tree */
+    struct texu_treewnd_notify
+    {
+        texu_wnd_notify hdr; /* notification header */
+        texu_tree_item *item;
+    };
+    typedef struct texu_treewnd_notify texu_treewnd_notify;
 
-struct texu_treewnd_item
-{
-    texu_char   itemtext[TEXU_MAX_WNDTEXT + 1];
-    texu_i64    lparam;
-    texu_bool   expanded;
-    texu_bool   selected;
-    texu_i32    children;
-    texu_bool   populated;
-    texu_i32    normcolor;
-    texu_i32    discolor;
-    texu_i32    selcolor;
-};
-typedef struct texu_treewnd_item texu_treewnd_item;
+    struct texu_msgbox_notify
+    {
+        texu_wnd_notify hdr; /* notification header */
+        texu_ui32 id;
+    };
+    typedef struct texu_msgbox_notify texu_msgbox_notify;
 
+    /*MENU*/
 
-struct texu_treewnd_notify
-{
-  texu_wnd_notify  hdr;         /* notification header */
-  texu_tree_item*  item;
-};
-typedef struct texu_treewnd_notify texu_treewnd_notify;
+    struct texu_menu_item
+    {
+        texu_ui32 id;
+        texu_char text[TEXU_MAX_WNDTEXT + 1];
+        texu_bool enable;
+        texu_i32 normcolor;
+        texu_i32 discolor;
+        texu_i32 selcolor;
+        texu_ui32 style; /*TEXU_MS_TEXT, TEXU_MS_BREAK*/
+        texu_char info[TEXU_MAX_WNDTEXT + 1];
+        void *userdata;
+    };
+    typedef struct texu_menu_item texu_menu_item;
 
-struct texu_msgbox_notify
-{
-  texu_wnd_notify  hdr;         /* notification header */
-  texu_ui32        id;
-};
-typedef struct texu_msgbox_notify texu_msgbox_notify;
+    struct texu_menu;
+    typedef struct texu_menu texu_menu;
 
+    struct texu_menuitem_notify
+    {
+        texu_wnd_notify hdr; /* notification header */
+        texu_ui32 id;
+        texu_char info[TEXU_MAX_WNDTEXT + 1];
+    };
+    typedef struct texu_menuitem_notify texu_menuitem_notify;
 
-/*MENU*/
+    struct texu_wnd_template
+    {
+        const texu_char *text;
+        const texu_char *clsname;
+        texu_ui32 style;
+        texu_ui32 exstyle;
+        texu_i32 y;
+        texu_i32 x;
+        texu_i32 h;
+        texu_i32 w;
+        texu_i32 id;
+    };
+    typedef struct texu_wnd_template texu_wnd_template;
 
-struct texu_menu_item
-{
-  texu_ui32       id;
-  texu_char       text[TEXU_MAX_WNDTEXT+1];
-  texu_bool       enable;
-  texu_i32        normcolor;
-  texu_i32        discolor;
-  texu_i32        selcolor;
-  texu_ui32       style;    /*TEXU_MS_TEXT, TEXU_MS_BREAK*/
-  texu_char       info[TEXU_MAX_WNDTEXT+1];
-  void*           userdata;
-};
-typedef struct texu_menu_item texu_menu_item;
+    struct texu_menuitem_template
+    {
+        const texu_char *text;
+        texu_ui32 id;
+        texu_i32 enabled;
+        const texu_char *info;
+    };
+    typedef struct texu_menuitem_template texu_menuitem_template;
 
-struct texu_menu;
-typedef struct texu_menu texu_menu;
-
-struct texu_menuitem_notify
-{
-  texu_wnd_notify   hdr;         /* notification header */
-  texu_ui32         id;
-  texu_char         info[TEXU_MAX_WNDTEXT+1];
-};
-typedef struct texu_menuitem_notify texu_menuitem_notify;
-
-struct texu_wnd_template
-{
-  const texu_char* text;
-  const texu_char* clsname;
-  texu_ui32  style;
-  texu_ui32  exstyle;
-  texu_i32   y;
-  texu_i32   x;
-  texu_i32   h;
-  texu_i32   w;
-  texu_i32   id;
-};
-typedef struct texu_wnd_template texu_wnd_template;
-
-
-struct texu_menuitem_template
-{
-  const texu_char*  text;
-  texu_ui32         id;
-  texu_i32          enabled;
-  const texu_char*  info;
-};
-typedef struct texu_menuitem_template texu_menuitem_template;
-
-#define TEXU_MAX_MENUITEMS    32
-struct texu_menu_template
-{
-  const texu_char*  text;
-  texu_i32          enabled;
-  const texu_char*  info;
-  texu_i32          nitems;
-  texu_menuitem_template   items[TEXU_MAX_MENUITEMS];
-};
-typedef struct texu_menu_template texu_menu_template;
-
+#define TEXU_MAX_MENUITEMS 32
+    struct texu_menu_template
+    {
+        const texu_char *text;
+        texu_i32 enabled;
+        const texu_char *info;
+        texu_i32 nitems;
+        texu_menuitem_template items[TEXU_MAX_MENUITEMS];
+    };
+    typedef struct texu_menu_template texu_menu_template;
 
 #ifdef __cplusplus
 }
 #endif
-
-
-
 
 #endif /*_TEXUSTRUCT_H_*/
--- include/texutils.h
+++ include/texutils.h
@@ -111,6 +111,248 @@ struct texu_db;
 typedef struct texu_db texu_db;
 
 
+/*
+# TexU safedb
+#
+# This object is required sqlite. It wraps around sqlite3 API
+#
+#
+         1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+*/
+
+
+#define TEXU_SAFEDB_BREAK        ( 2)
+#define TEXU_SAFEDB_NOROW        ( 1)
+#define TEXU_SAFEDB_OK           ( 0)
+#define TEXU_SAFEDB_ERROR        (-1)
+#define TEXU_SAFEDB_EMEM         (-2)
+#define TEXU_SAFEDB_ESQL         (-3)
+#define TEXU_SAFEDB_ECONSTRUCTOR (-4)
+#define TEXU_SAFEDB_EOUTBOUND    (-5)
+
+#define TEXU_SAFEDB_FAILED(e)    ((e) <  0)
+#define TEXU_SAFEDB_SUCCEEDED(e) ((e) >= 0)
+
+#define TEXU_SAFEDB_TRUE         (1)
+#define TEXU_SAFEDB_FALSE        (0)
+
+#define TEXU_SAFEDB_NULL         ((void*)0)
+#define TEXU_SAFEDB_ISNULL(p)    (!(p))
+
+struct _texu_safedb
+{
+    sqlite3 *db;
+};
+typedef struct _texu_safedb texu_safedb_t;
+typedef struct _texu_safedb *texu_safedb_ptr; /*db*/
+typedef void*  texu_safedb_rowptr;       /*row ptr*/
+typedef void*  texu_safedb_userptr;      /*user ptr*/
+
+typedef char                    texu_safedb_i8;
+typedef short                   texu_safedb_i16;
+typedef int                     texu_safedb_i32;
+typedef long long               texu_safedb_i64;
+typedef unsigned char           texu_safedb_ui8;
+typedef unsigned short          texu_safedb_ui16;
+typedef unsigned int            texu_safedb_ui32;
+typedef unsigned long long      texu_safedb_ui64;
+typedef float                   texu_safedb_f32;
+typedef double                  texu_safedb_f64;
+
+typedef unsigned char           texu_safedb_byte;
+typedef char                    texu_safedb_char;
+typedef texu_safedb_i64              texu_safedb_int;
+typedef texu_safedb_f64              texu_safedb_float;
+typedef texu_safedb_i32              texu_safedb_status;
+typedef const char*             texu_safedb_cstr;
+typedef char*                   texu_safedb_str;
+typedef texu_safedb_i32              texu_safedb_bool;
+
+
+texu_safedb_ptr  texu_safedb_new();
+void        texu_safedb_del(texu_safedb_ptr db);
+
+texu_safedb_status   texu_safedb_open(texu_safedb_ptr db, texu_safedb_cstr dbfile);
+texu_safedb_status   texu_safedb_open2(texu_safedb_ptr db, texu_safedb_cstr dbfile, texu_safedb_ui32 flags);
+texu_safedb_status   texu_safedb_open_read(texu_safedb_ptr db, texu_safedb_cstr dbfile);
+void            texu_safedb_close(texu_safedb_ptr db);
+
+/*****************************************************************************
+CREATE TABLE student(
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT,
+    age     REAL,
+    height  REAL,
+    weight  REAL
+    );
+
+INSERT INTO student(name,age,height,weight) VALUES('smith',12.5,157.2,58.2);
+INSERT INTO student(name,age,height,weight) VALUES('neo',12.3,158.2,52.3);
+INSERT INTO student(name,age,height,weight) VALUES('trinity',12.6,162.2,55.8);
+INSERT INTO student(name,age,height,weight) VALUES('morphoues',13.5,163.9,65.3);
+INSERT INTO student(name,age,height,weight) VALUES('oracle',33.8,160.9,60.3);
+INSERT INTO student(name,age,height,weight) VALUES('tank',10.5,152.9,45.3);
+
+UPDATE student SET name='smith' WHERE id=1;
+
+DELETE student WHERE id=6;
+*****************************************************************************/
+/* See sqlite3 document*/
+typedef int (*texu_safedb_exec_callback)(void* userptr, int argc, char **argv, char **name);
+int         texu_safedb_exec(texu_safedb_ptr db, texu_safedb_cstr sqlcmd, texu_safedb_exec_callback callback, texu_safedb_userptr userptr);
+
+/*****************************************************************************
+Example to call copy from callback function
+struct _student
+{
+    long    id;
+    char    name[32];
+    double  age;
+    double  height;
+    double  weight;
+};
+typedef struct _student student_t;
+
+texu_safedb_status copy_student_callback(texu_safedb_userptr userptr, texu_safedb_i32 col, const texu_safedb_byte* value)
+{
+    student_t *student = (student_t*)userptr;
+    char* val = (char*)value;
+    switch(col)
+    {
+        case 1: strcpy(student->name, (val ? val   : ""));      break;
+        case 0: student->id     = (val ? atol(val) : 0);        break;
+        case 2: student->age    = (val ? atof(val) : 0);        break;
+        case 3: student->height = (val ? atof(val) : 0);        break;
+        case 4: student->weight = (val ? atof(val) : 0);        break;
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    const char *sqlcmd = "SELECT * FROM student WHERE AGE > 7;";
+    texu_safedb_ptr  db = texu_safedb_new();
+    texu_safedb_status rc = texu_safedb_open(db, "./student.db");
+    if (TEXU_SAFEDB_OK != rc)
+    {
+        return 1;
+    }
+
+    texu_safedb_sel_ptr selptr = texu_safedb_select(db, sqlcmd, copy_student_callback, sizeof(student_t));
+    if (selptr)
+    {
+        if (selptr->nitems > 0 && selptr->items)
+        {
+            texu_safedb_ui32 rows = selptr->nitems;
+            student_t *student = 0;
+            texu_safedb_i32 i = 0;
+            for (i = 0; i < rows; ++i)
+            {
+                student = (student_t *)texu_safedb_sel_get(selptr, i);
+                printf("id:%ld, name:'%s', height: %f, weight: %f\n",
+                    student->id, student->name, student->height, student->weight);
+            }
+        }
+        else
+        {
+            printf("data not found\n");
+        }
+    }
+    else
+    {
+        printf("statement is incorrect or table not found\n");
+    }
+
+    texu_safedb_close(db);
+    texu_safedb_del(db);
+    return 0;
+}
+*****************************************************************************/
+/*
+texu_safedb_status texu_safedb_copy_callback(P1, P2, P3)
+PURPOSE:
+    To copy a row data from DB by calling texu_safedb_select()
+
+RETURN:
+    TEXU_SAFEDB_OK to continue copy data from DB. Otherwise break the copy from DB.
+
+PARAMETERS:
+    P1 - to copy for each row from DB
+    P2 - the DB column index by the zero based
+    P3 - the DB value for each column returned by selecting
+*/
+typedef texu_safedb_status (*texu_safedb_copy_callback)(texu_safedb_userptr userptr, texu_safedb_i32 col, const texu_safedb_byte* value);
+
+struct _texu_safedb_select
+{
+    texu_safedb_ui32     size;   /*size of an item per record*/
+    texu_safedb_ui32     nrows;  /*a number of rows*/
+    texu_safedb_rowptr   items;  /*an array of items*/
+    texu_safedb_ui32     nitems; /*an actual items, it may not be equal nrows*/
+                            /*it depends on the implementation of callback*/
+                            /*it always not be greater than nrows*/
+};
+typedef struct _texu_safedb_select texu_safedb_sel_t;
+typedef struct _texu_safedb_select *texu_safedb_sel_ptr;
+/*****************************************************************************
+texu_safedb_select() would return an array of records
+SQL Example:
+    const char *sqlcmd = "SELECT * FROM STUDENT_TABLE WHERE AGE > 7;";
+
+Return:
+    texu_safedb_sel_ptr if sqlcmd was successfully called. Please double-check nrows and items if there
+    is no row returned.
+    Otherwise, NULL if sqlcmd is incorrect statement. Such as no table in database and so on.
+
+Example:
+    texu_safedb_sel_ptr selptr = texu_safedb_select(db, sqlcmd, copy_student_callback, sizeof(student_t));
+    if (selptr)
+    {
+        if (selptr->nitems > 0 && selptr->items)
+        {
+            printf("data found\n");
+        }
+        else
+        {
+            printf("data not found\n");
+        }
+    }
+    else
+    {
+        printf("statement is incorrect or table not found\n");
+    }
+*****************************************************************************/
+texu_safedb_sel_ptr  texu_safedb_select(texu_safedb_ptr db, texu_safedb_cstr sqlcmd, texu_safedb_copy_callback callback, texu_safedb_ui32 size);
+/*
+texu_safedb_sel_get() to read a specific record with a zero base
+Example:
+    student_t *student = texu_safedb_sel_get(selptr, 0);
+*/
+texu_safedb_rowptr   texu_safedb_sel_get(texu_safedb_sel_ptr selptr, texu_safedb_i32 row);
+/*
+texu_safedb_sel_free() to release texu_safedb_sel_ptr object
+*/
+void            texu_safedb_sel_free(texu_safedb_sel_ptr selptr);
+/*****************************************************************************
+texu_safedb_aggregate() would only one aggregate SQL function
+SQL Example:
+    const char *sqlcmd1 = "SELECT COUNT(*) FROM student;";
+    const char *sqlcmd2 = "SELECT AVG(age) FROM student;";
+    const char *sqlcmd3 = "SELECT MAX(age) FROM student;";
+    const char *sqlcmd4 = "SELECT SUM(age) FROM student;";
+
+Example:
+    long    count_students  = (long)texu_safedb_aggregate(db, sqlcmd1);
+    double  avg_age         = texu_safedb_aggregate(db, sqlcmd2);
+    double  max_age         = texu_safedb_aggregate(db, sqlcmd3);
+    double  sum_age         = texu_safedb_aggregate(db, sqlcmd4);
+*****************************************************************************/
+texu_safedb_float    texu_safedb_aggregate(texu_safedb_ptr db, texu_safedb_cstr sqlcmd);
+
+texu_safedb_status   texu_safedb_last_errno();
+texu_safedb_bool     texu_safedb_is_existing(texu_safedb_ptr db, texu_safedb_cstr tabname);
+
 
 #ifdef __cplusplus
 }
