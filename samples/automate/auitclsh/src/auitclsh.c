1d1d11f5188c9054bbfece5b19c318264fe4c454
Seree Rakwong <meo.rakwong@hotmail.com>
2022-11-22 06:55:50 +0700

    Update auitclsh.c

--- samples/automate/auitclsh/src/auitclsh.c
+++ samples/automate/auitclsh/src/auitclsh.c
@@ -14,13 +14,8 @@
 
 #include "auitclsh.h"
 
-auitclsh_t g_auitclsh;
-
 int main(int argc, char *argv[])
-{
-    g_auitclsh.msgkey = ftok("autoui", 65); /*the name is the same as TexU server*/
-    g_auitclsh.msgid  = msgget(g_auitclsh.msgkey, 0666 | IPC_CREAT);
-    
+{   
     /*tcl*/
     Tcl_Main( argc, argv, auitclsh_init );
     return 0;
