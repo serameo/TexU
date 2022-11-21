6c9653ee5ccb466cad43fd21ae474eca01012e98
Seree Rakwong <meo.rakwong@hotmail.com>
2022-11-21 16:56:40 +0700

    Create auitclsh.c

---
+++ samples/automate/auitclsh/src/auitclsh.c
@@ -0,0 +1,34 @@
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tcl.h>
+#include <texu.h>
+
+#include "auitclsh.h"
+
+auitclsh_t g_auitclsh;
+
+int
+atrtcl_init(
+  Tcl_Interp* interp
+);
+
+
+int main(int argc, char *argv[])
+{
+    g_auitclsh.msgkey = ftok("autoui", 65); /*the name is the same as TexU server*/
+    g_auitclsh.msgid  = msgget(g_atrtcl.msgkey, 0666 | IPC_CREAT);
+    
+    /*tcl*/
+    Tcl_Main( argc, argv, auitclsh_init );
+    return 0;
+}
+
