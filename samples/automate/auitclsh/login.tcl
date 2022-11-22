df6135637190b8a208afcee0925456f68413ae83
Seree Rakwong <meo.rakwong@hotmail.com>
2022-11-22 08:50:00 +0700

    Update login.tcl

--- samples/automate/auitclsh/login.tcl
+++ samples/automate/auitclsh/login.tcl
@@ -1,8 +1,8 @@
 #!./auitclsh
 
 # set user
-atr_settext 10008 1000
-set user [atr_gettext 10008]
+aui_settext 10008 1000
+set user [aui_gettext 10008]
 if {$user == "1000"} {
   puts "set user OK"
 } else {
@@ -10,16 +10,17 @@ if {$user == "1000"} {
 }
 
 # set passwd 
-atr_settext 10009 H1W0RLD
-set passwd [atr_gettext 10009]
+aui_settext 10009 H1W0RLD
+set passwd [aui_gettext 10009]
 if {$passwd == "H1W0RLD"} {
   puts "set password OK"
 } else {
   puts "set password failed"
 }
 
-atr_sendmsg -1 12 10001
-set info [atr_gettext 10011]
+# send command (12) ID_LOGIN (10001)
+aui_sendmsg -1 12 10001
+set info [aui_gettext 10011]
 if {$info == "Login OK"} {
   puts "Login successfully"
 } else {
