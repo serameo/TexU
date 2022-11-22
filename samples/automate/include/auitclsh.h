644471f6112edd857638cd6560995b3fe3fb2c48
Seree Rakwong <meo.rakwong@hotmail.com>
2022-11-22 14:43:41 +0700

    Add files via upload

--- samples/automate/include/auitclsh.h
+++ samples/automate/include/auitclsh.h
@@ -1,1 +1,52 @@
+#ifndef _AUITCLSH_H_
+#define _AUITCLSH_H_
 
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include "tcl.h"
+#include "texu.h"
+#include "cJSON.h"
+
+struct _auitclsh
+{
+    int         msgid;
+    key_t       msgkey;
+    texu_xcnf   *xcnf;
+};
+typedef struct _auitclsh auitclsh_t;
+
+/*initial Tcl command interfaces*/
+int
+auitclsh_init(
+  Tcl_Interp* interp
+);
+
+/* Send message to the autoui app*/
+int
+aui_sendmsg(
+  ClientData      cd,
+  Tcl_Interp*     interp,
+  int             objc,
+  Tcl_Obj *CONST  objv[]
+);
+
+/* set text to a window object in the autoui app*/
+int
+aui_settext(
+  ClientData      cd,
+  Tcl_Interp*     interp,
+  int             objc,
+  Tcl_Obj *CONST  objv[]
+);
+
+/* get text from a window object in the autoui app*/
+int
+aui_gettext(
+  ClientData      cd,
+  Tcl_Interp*     interp,
+  int             objc,
+  Tcl_Obj *CONST  objv[]
+);
+
+#endif /*_AUITCLSH_H_*/
