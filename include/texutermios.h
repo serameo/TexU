30d3d983bb11df8dd8462ef0fb3baaa4653bc2a3
Seree Rakwong <meo.rakwong@hotmail.com>
2025-01-02 10:00:55 +0700

    Add files via upload

--- include/texutermios.h
+++ include/texutermios.h
@@ -1,22 +1,29 @@
 #ifndef __TEXU_TERMIOS_H__
 #define __TEXU_TERMIOS_H__
 
+#if defined __USE_TERMIOS__
+
 #include "../termbox/termbox.h"
 
-#include "texutermios.h"
+struct texu_cio;
 #if defined __USE_TERMIOS__
+typedef void (*fn_signal_callback)(int sig, void*);
+typedef void (*fn_sigint_callback)(void*);
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 int     texu_termios_init(int lines, int cols);
 void    texu_termios_release();
-int     texu_termios_getch(struct tb_event *ev, int timeout);
+int     texu_termios_getch(struct texu_cio *cio, struct tb_event *ev, int timeout);
 void    texu_termios_gotoyx(int y, int x);
 void    texu_termios_clear();
-int     texu_termios_putch(int y, int x, char ch, uint16_t fg, uint16_t bg);
-int     texu_termios_putstr(int y, int x, const char* str, uint16_t fg, uint16_t bg);
+int     texu_termios_putch(int y, int x, char ch, unsigned short fg, unsigned short bg);
+int     texu_termios_putstr(int y, int x, const char* str, unsigned short fg, unsigned short bg);
 int     texu_termios_refresh();
+void    texu_termios_cbreak(struct texu_cio *cio, int cbreak);/*, fn_sigint_callback sigint_handler, void *userdata);*/
+void    texu_termios_getyx(int *y, int *x);
 
 #ifdef __cplusplus
 }
